b1f52b89b86d0deeceadc9171adf5fac3da43882
dbashyal@gmail.com <dbashyal@gmail.com@b306fca2-2ad7-1eb3-9d6e-0d44a79928db>
2011-08-17 10:59:51 +0000

    added blog_lang.php

---
+++ trunk/codefight-cms/codefight/app/language/english/blog_lang.php
@@ -0,0 +1,37 @@
+ <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeFight
+ * Date: 14/08/11
+ * Time: 7:17 AM
+ *
+ * An open source application
+ *
+ * @package		CodeFight CMS
+ * @author		Damodar Bashyal
+ * @copyright	Copyright (c) 2010, Codefight.org
+ * @license		Pending
+ * @link		http://codefight.org
+ * @since		Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Codefight CMS Blog Language
+ *
+ * @package		CodeFight
+ * @subpackage	Language
+ * @category	Language
+ * @author		Damodar Bashyal
+ * @link		Pending Doc Link
+ */
+ $lang['Static Page'] = 'Static Page';
+ $lang['Select'] = 'Select';
+ $lang['Title'] = 'Title';
+ $lang['Websites'] = 'Websites';
+ $lang['Delete Selected'] = 'Delete Selected';
+ $lang['Edit Selected'] = 'Edit Selected';
+ $lang['Create New Page'] = 'Create New Page';
+ $lang['Reset'] = 'Reset';
+ $lang['Blog'] = 'Blog';
